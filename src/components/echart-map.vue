d668220c198ef2b6da5bf79470405d239f1dba42
JHFeng <lingj.jhf@outlook.com>
2023-05-30 19:59:16 +0800

    开发代码

--- src/components/echart-map.vue
+++ src/components/echart-map.vue
@@ -1,15 +1,62 @@
 <template>
-  <div id="echarts-map"></div>
+  <div id="echarts-map" ref="mapRef" class="h-200px w-200px"></div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import * as echarts from 'echarts/core'
+import { onMounted, shallowRef } from "vue";
+import * as echarts from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { MapChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+} from "echarts/components";
+import { LabelLayout, UniversalTransition } from "echarts/features";
+import type { MapSeriesOption } from "echarts/charts";
+import type { ComposeOption } from "echarts/core";
+import guangdong from "@/data/guangdong.json";
+import type { FeatureCollection } from "geojson";
 
-onMounted(()=>{
-  echarts.init()
-})
+function transformPolylineStringToGeoJson(polyline: string): FeatureCollection {
 
+  return {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: [],
+        geometry: { type: "", coordinates: [] },
+      },
+    ],
+  };
+}
+
+type Options = ComposeOption<MapSeriesOption>;
+
+const mapRef = shallowRef<HTMLDivElement>();
+echarts.use([
+  MapChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+]);
+onMounted(() => {
+  if (mapRef.value) {
+    const json = transformPolylineStringToGeoJson(guangdong.polyline);
+
+    echarts.registerMap("china", json as any);
+    const echart = echarts.init(mapRef.value);
+
+    const options: Options = {};
+    echart.setOption(options);
+  }
+});
 </script>
-
-<style scoped></style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>home</div>
+  <div>
+    <EchartMap></EchartMap>
+  </div>
 </template>
 
 <script setup lang="ts">
-
-  </script>
+import EchartMap from "@/components/echart-map.vue";
+</script>
 
 <style scoped></style>
