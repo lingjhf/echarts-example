e29158c9d85cee384330ebc41263fd7f59b624c8
JHFeng <lingj.jhf@outlook.com>
2023-05-30 18:52:26 +0800

    init

---
+++ src/components/echart-map.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="echarts-map"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import * as echarts from 'echarts/core'
+
+onMounted(()=>{
+  echarts.init()
+})
+
+</script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+
+function resolve(p: string) {
+  return path.resolve(__dirname, p)
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), Unocss()],
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
+})
